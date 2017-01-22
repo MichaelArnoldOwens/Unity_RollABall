1c92c6de7174b06f3cbc6805acf8f6db82fba1e4
Michael Owens <xmichael.owens@gmail.com>
2017-01-22 15:16:48 -0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+  public float speed;
+
+  private Rigidbody rb;
+
+  // Called automatically when script is loaded
+  void Start() {
+    // Access Rigidbody component on the same game object
+    rb = GetComponent<Rigidbody>();
+  }
+
+  // Called before rendering a frame
+  void Update() {
+    
+  }
+
+  // Called before physics calculations
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+    rb.AddForce(movement * speed);
+  }
+
+  // OnTriggerEnter is called when the Collider other enters the trigger
+  void OnTriggerEnter(Collider other) {
+    // Destroy(other.gameObject);
+    if(other.gameObject.CompareTag("Pick Up")) {
+      other.gameObject.SetActive(false);
+    }
+  }
+}
